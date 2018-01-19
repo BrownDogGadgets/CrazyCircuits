6609246e084d143b1e151585ffba937bbfbdd96f
Joshua Zimmerman <joshua@browndoggadgets.com>
2018-01-18 18:39:09 -0600

    Update ConstantTone.ino

--- Projects/SquishySound/ConstantTone.ino
+++ Projects/SquishySound/ConstantTone.ino
@@ -1,8 +1,8 @@
 //Squishy Sound - Code written by Sam Johnson and Modified by Matthew Schmidtbauer for the Squishy Circuits Project
 //Port Definitions and Variable Declarations: 
-//Change the SpeakerOutput Pin as well as the Raw pin if you're using a Robotics board or standard Arduino board.
+//Change the SpeakerOutput Pin as well as the analog pin if you're using a Robotics board or standard Arduino board.
 #define SpeakerOutput 16
-int analog = 14; // Common resistor connected to analog pin 0 outside leads to ground and +5V 
+int analog = 14; // Common resistor connected to  pin 14 outside leads to ground and +5V 
 int raw = 0; // Variable to store the raw input value
 int frequency = 0; // Variable to store Frequency
 void setup() {}
