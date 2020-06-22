8eab088ec2c336c00a1db556008267b10b8ba577
Pete Prodoehl <pete@rasterweb.net>
2020-06-22 14:15:03 -0500

    Initial commit

---
+++ Projects/Avoidance Robot/Arduino/AvoidanceRobot/AvoidanceRobot.ino
@@ -0,0 +1,122 @@
+/*
+ * AvoidanceRobot.ino
+ * 
+ * Brown Dog Gadgets <https://www.browndoggadgets.com/>
+ * 
+ * We used the following items for this robot:
+ *   1 x Crazy Circuits Robotics Board <https://www.browndoggadgets.com/collections/tbm/products/crazy-circuits-robotics-board>
+ *   2 x LEGO Compatible Continuous Rotation 360 Degree Servo <https://www.browndoggadgets.com/collections/tbm/products/lego-compatible-360-degree-servo>
+ *   1 x HC-SR04 Ultrasonic Distance Sensor
+ *   1 x USB Power Bank
+ *
+ */
+
+
+
+// you will need the following libraries
+#include <NewPing.h>
+#include <Servo.h>
+
+// set up the ultrasonic sensor, connect the echo and trigger wires to the specified pins
+#define ECHO_PIN     3  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define TRIGGER_PIN  5  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define MAX_DISTANCE 800 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+int theDistance = 0;
+int theDistanceTotal = 0;
+int triggerDistance = 13; // set the distance in centimeters to trigger the action
+int sampleVal = 5;
+
+// make two servo objects
+Servo servoLeft;
+Servo servoRight;
+
+// connect the signal wires (orange) to the specified pins
+int servoPinLeft = 6;
+int servoPinRight = 9;
+
+// this is the midpoint of the servo, or where it is "stopped"
+int midPoint = 1600;
+
+// set the number for forward and backward movement
+int forward = 1300;
+int backward = 1800;
+
+
+// setup runs one time when things start
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println("Starting...");
+  servoLeft.attach(servoPinLeft);
+  servoRight.attach(servoPinRight);
+  delay(500);
+  
+}
+
+// loop runs forever
+void loop() {
+
+  delay(50); // delay between pings
+  theDistance = sonar.ping_cm();
+
+  // check if the distance sensor sees something
+  if ((theDistance > 0) and (theDistance < triggerDistance)) {
+    
+    // distance sensor
+    Serial.print("distance: ");
+    Serial.print(theDistance);
+    Serial.println("cm");
+    
+    goBackward(1000);
+    turnRight(1000);
+
+  }
+
+  
+  else {
+    
+    // if the distance sensor does not see anything, keep moving forward
+    goForward(100);
+
+  }
+
+}
+
+
+// Below are the functions to move forward & backward, and turn left & right
+
+void goForward(int theDelay) {
+
+  servoLeft.writeMicroseconds(backward);
+  servoRight.writeMicroseconds(forward);
+  delay(theDelay);
+  
+}
+
+
+void goBackward(int theDelay) {
+
+  servoLeft.writeMicroseconds(forward);
+  servoRight.writeMicroseconds(backward);
+  delay(theDelay);
+  
+}
+
+
+void turnLeft(int theDelay) {
+
+  servoLeft.writeMicroseconds(forward);
+  servoRight.writeMicroseconds(forward);
+  delay(theDelay);
+  
+}
+
+void turnRight(int theDelay) {
+
+  servoLeft.writeMicroseconds(backward);
+  servoRight.writeMicroseconds(backward);
+  delay(theDelay);
+  
+}
---
+++ Projects/Avoidance Robot/README.md
@@ -0,0 +1,36 @@
+# Avoidance Robot
+
+![Avoidance Robot](Images/avoidance-robot.jpg)
+
+An Avoidance Robot built with Crazy Circuits components and LEGO parts.
+
+Electronic Parts Needed:
+* 1 x [Crazy Circuits Robotics Board](https://www.browndoggadgets.com/collections/new-crazy-circuits-kits/products/crazy-circuits-robotics-board)
+* 2 x [LEGO Compatible Continuous Rotation 360 Degree Servo](https://www.browndoggadgets.com/collections/tbm/products/lego-compatible-360-degree-servo)
+* 1 x HC-SR04 Ultrasonic Distance Sensor
+* 1 x USB Power Bank
+
+We used the following LEGO parts, but YMMV:
+
+* 1 x [LEGO Wedge Belt Wheel (4185 / 49750)](https://www.brickowl.com/catalog/lego-wedge-belt-wheel-4185-49750)
+* 1 x [LEGO EV3 Technic Ball Pivots Set 5003245](https://www.brickowl.com/catalog/lego-ev3-technic-ball-pivots-set-5003245)
+* 1 x [LEGO Technic Cross Block Beam 3 with Four Pins (48989 / 65489)](https://www.brickowl.com/catalog/lego-technic-cross-block-beam-3-with-four-pins-48989-65489)
+* 1 x [LEGO Technic Brick 1 x 6 with Holes (3894)](https://www.brickowl.com/catalog/lego-technic-brick-1-x-6-with-holes-3894)
+* 4 x [LEGO Axle 4 with End Stop (87083)](https://www.brickowl.com/catalog/lego-axle-4-with-end-stop-87083)
+* 4 x [LEGO Half Bushing (32123 / 42136)](https://technicforall.brickowl.com/store/lego-half-bushing-32123-42136)
+* 4 x [LEGO Brick 2 x 2 Round (3941 / 6143)](https://technicforall.brickowl.com/store/lego-brick-2-x-2-round-3941-6143)
+* 1 x [LEGO Plate 6 x 12 (3028)](https://www.brickowl.com/catalog/lego-plate-6-x-12-3028)
+* Variety of micellaneous LEGO pieces
+
+
+The Arduino sketch `AvoidanceRobot` is included.
+
+You will also need the NewPing library, which can be found here: https://bitbucket.org/teckel12/arduino-new-ping/wiki/Home
+
+
+---
+
+Brown Dog Gadgets
+
+https://www.browndoggadgets.com/
+
