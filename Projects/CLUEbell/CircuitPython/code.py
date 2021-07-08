a425a08a5f16950ef3e73c73a122b3321e9f7b72
Pete Prodoehl <pete@rasterweb.net>
2021-07-08 11:07:27 -0500

    Update code.py

--- Projects/CLUEbell/CircuitPython/code.py
+++ Projects/CLUEbell/CircuitPython/code.py
@@ -6,31 +6,26 @@ import pulseio
 from adafruit_motor import servo
 from adafruit_clue import clue
 
-pwm = pulseio.PWMOut(board.P0, duty_cycle=2 ** 15, frequency=50)
+pwm = pulseio.PWMOut(board.P13, duty_cycle=2 ** 15, frequency=50)
 
 my_servo = servo.Servo(pwm)
 
-button = digitalio.DigitalInOut(board.P1)
+my_servo.angle = 80
+
+button = digitalio.DigitalInOut(board.P8)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
-relay = digitalio.DigitalInOut(board.P16)
-relay.direction = digitalio.Direction.OUTPUT
-
-my_servo.angle = 90
-
-print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+print("\n\n\n\n\n\n\n\n\n\n\n")
 
 while True:
-    print("Waiting for Bubbles...\n\n\n\n\n\n\n\n")
+    print("I've got a fever...\n\n\n\n\n\n\n\n")
     if button.value < 1:
-        print("Prepare for Bubbles!\n\n\n\n\n\n\n\n")
-        time.sleep(0.5)
-        my_servo.angle = 145
-        time.sleep(2)
-        my_servo.angle = 118
-        relay.value = True
-        time.sleep(1)
-        relay.value = False
-        my_servo.angle = 90
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+		print("I've got a fever...\n\n")
+		print("And the only prescription is...\n\n")
+		print("MORE COWBELL!!!\n\n")
+		time.sleep(0.5)
+		my_servo.angle = 115
+		time.sleep(0.06)
+		my_servo.angle = 80
+		time.sleep(0.08)
